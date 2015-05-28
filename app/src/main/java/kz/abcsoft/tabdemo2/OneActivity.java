abf2cce074070c5f690cdda1dc47c330a1116ef7
Daulet Baimbetov <daucloudlab@gmail.com>
2015-05-28 18:21:05 +0600

    Simple Tab demo project

---
+++ app/src/main/java/kz/abcsoft/tabdemo2/OneActivity.java
@@ -0,0 +1,12 @@
+package kz.abcsoft.tabdemo2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OneActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.one);
+    }
+}
